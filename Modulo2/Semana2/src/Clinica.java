bcef88bd54fbc35a45b7dab1946cea90a56a1ed0
Adler-Hamann <adlerhamannmartins@gmail.com>
2023-03-09 22:00:06 -0300

    Exercicio 2 da semana 3

--- Modulo2/Semana2/src/Clinica.java
+++ Modulo2/Semana2/src/Clinica.java
@@ -4,4 +4,9 @@ public class Clinica {
     private String cnpj;
     private String logradouro;
     private String codclinica;
+
+    public static double IMC(double peso, double altura){
+        double imc = peso / (altura * altura);
+        return imc;
+    }
 }
